9480e2af779d91bd1d94dbdcf896c5e095054c69
Paulo Carvalho <paulocarvalhojr@gmail.com>
2024-06-10 11:39:38 -0300

    Renaming project to be compatible with the nuget repository

--- src/AwsTool.Sdk/AwsDefaultSettings.cs
+++ src/AwsTool.Sdk/AwsDefaultSettings.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Aws.Sdk.Toolkit;
+namespace AwsTool.Sdk;
 
 [ExcludeFromCodeCoverage]
 public static class AwsDefaultSettings
