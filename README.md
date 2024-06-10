cfadbee8c3ef8c1fbf2483ee205295d4ea38390e
Paulo Carvalho <paulocarvalhojr@gmail.com>
2024-06-10 10:05:10 -0300

    Create project
    Creating base projects with resources shared between integrations with AWS

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# aws-sdk-tookit
+# aws-sdk-toolkit
 Responsible for delivering the minimum requirements for integration with AWS services.
---
+++ src/Aws.Sdk.Toolkit/AwsDefaultSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Aws.Sdk.Toolkit;
+
+[ExcludeFromCodeCoverage]
+public static class AwsDefaultSettings
+{
+    public static IServiceCollection AddAwsDefaultSettings(this IServiceCollection services,
+        IConfiguration configuration, IHostEnvironment environment)
+    {
+        if (!environment.IsDevelopment())
+            services.AddDefaultAWSOptions(configuration.GetAWSOptions());
+
+        return services;
+    }
+}
